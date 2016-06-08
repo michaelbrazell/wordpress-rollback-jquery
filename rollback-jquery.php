ef1e55c4eab0a5f5970b2bfea7bf580cd00d7647
Mike Murray <michaelbrazell@gmail.com>
2016-06-08 19:56:24 -0400

    Create plugin and dependent files for version 1.0

---
+++ rollback-jquery.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Rollback jQuery
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: This plugin rolls jQuery 1.12.3 (or any other version) back to 1.11.3 from Google CDN.  It should only be used if you have a compatibility error with the latest version of jQuery bundled in Core.
+Version:     1.0
+Author:      Mike Murray
+Author URI:  http://www.worcesterwebgroup.com
+License:     MIT
+License URI: https://opensource.org/licenses/MIT
+*/
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "jquery_rollback", 11);
+function jquery_rollback() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") .
+        "://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
